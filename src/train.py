0e0ad35a21551e2fc7896a1f564961da3d852e03
aiaja <aisyafirst@gmail.com>
2025-02-19 08:24:35 +0700

    class balancing smote

--- src/train.py
+++ src/train.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE
 
 # Load Preprocessed Data
 dataset = pd.read_csv('dataset/clean_dataset_part01.csv', sep=';')
@@ -14,15 +15,28 @@ y = dataset['category']
 # Train-Test Split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
+# Load TF-IDF
+Tfidf_Vectorizer = TfidfVectorizer()
+X_train_vec = Tfidf_Vectorizer.fit_transform(X_train)
+
+# SMOTE
+smote = SMOTE(random_state=42)
+X_train_resampled, y_train_resampled = smote.fit_resample(X_train_vec, y_train)
+
 # Pipeline: TF-IDF + Naive Bayes
 pipeline = Pipeline([
-    ('tfidf', TfidfVectorizer(max_df=0.95, min_df=5)),
+    ('tfidf', TfidfVectorizer()),
     ('model', MultinomialNB())
 ])
 
 # Hyperparameter Tuning (Grid Search)
-params = {'model__alpha': [0.01, 0.1, 0.5, 1, 5]}
-model = GridSearchCV(pipeline, param_grid=params, cv=5, scoring='accuracy')
+params = {
+    'tfidf__max_df': [0.75, 1.0],
+    'tfidf__min_df': [1, 3],
+    'tfidf__ngram_range': [(1, 1), (1, 2)],
+    'model__alpha': [0.1, 0.5, 1.0]
+}
+model = GridSearchCV(pipeline, params, cv = 5, scoring='f1_weighted', verbose=2, n_jobs=-1)
 model.fit(X_train, y_train)
 
 # Simpan Model ke Folder 'models'
